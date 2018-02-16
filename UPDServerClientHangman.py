ee75bb4e77a5308ab1371415078e66f5ae30a371
Randolph Wilson III <randy.vashthestampede@gmail.com>
2018-02-16 13:32:42 -0500

    Added server portion for debugging

--- UPDServerClientHangman.py
+++ UPDServerClientHangman.py
@@ -13,11 +13,10 @@ def client():
 
     print("UDP target ip:", UDP_IP)
     print("UDP target port:", UDP_Port)
-    print("Message:", Message)
-
+    #print("Message:", Message)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     sock.sendto(Message.encode(), (UDP_IP, UDP_Port))
+    return Message
 
 
 def host(): #here for the purposes of debugging my own code and only having to deploy one file to my raspis
@@ -30,6 +29,24 @@ def host(): #here for the purposes of debugging my own code and only having to d
         data, addr = sock.recvfrom(1024)
         print("Received message:", data)
         print("Client IP address:", addr[0])
+
+def gameDebug():
+    x = True
+
+    while x:
+        Choice = input("Are you (1)hosting or (2)playing hangman?, enter (0) to quit")
+        if Choice == 1: #server
+            pass
+        if Choice == 2:
+            msg = client()
+            while str(msg).lower() != ('you win' and 'you lose'):
+                print("Word is ", msg[0], " characters long")
+                print(msg[1:len(msg)])
+                guess = input('Please guess a letter').lower()
+
+        if Choice == 0:
+            break
+
 
 #client()
 getIP()
